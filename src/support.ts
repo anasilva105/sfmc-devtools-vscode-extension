9960800a49190dec587a259d111dcb44d22eb19c
tulio.natale@accenture.com <tulio.natale@accenture.com>
2022-09-14 12:58:18 +0200

    working help commands

--- src/support.ts
+++ src/support.ts
@@ -8,6 +8,8 @@ export async function support(context: ExtensionContext) {
 
     interface State {
         helpCommand: QuickPickItem;
+		gitUsername: string;
+		gitEmail: string;
         title: string;
         step: number;
         totalSteps: number;
@@ -23,13 +25,14 @@ export async function support(context: ExtensionContext) {
 
 
     const title = 'DevTools Help';
-
+	const terminal = window.createTerminal(`Help`);
 
     async function pickHelpCommand(
         input: MultiStepInput,
         state: Partial < State >
     ) {
         const options: QuickPickItem[] = [
+			"upgrade mcdev",
             "check version mcdev",
             "open extension documentation",
             "open mcdev documentation",
@@ -48,11 +51,10 @@ export async function support(context: ExtensionContext) {
                 undefined,
            shouldResume: shouldResume,
         });
-        const terminal = window.createTerminal(`Help`);
 		terminal.show();
         switch (state.helpCommand.label) {
             case "upgrade mcdev":
-                terminal.sendText(`mcdev upgrade --y.credentialsName --y.credentialsName`);
+              return (input: MultiStepInput) => inputGitUsername(input, state);
                 break;
             case "check version mcdev":
                 terminal.sendText('mcdev --version');
@@ -67,11 +69,63 @@ export async function support(context: ExtensionContext) {
         return;
 	}
 
+	async function inputGitUsername(
+		input: MultiStepInput,
+		state: Partial<State>
+	  ) {
+		state.gitUsername = await input.showInputBox({
+		  ignoreFocusOut: true,
+		  title,
+		  step: 1,
+		  totalSteps: 2,
+		  value: state.gitUsername || "",
+		  prompt: "Enter Git Username",
+		  validate: validateUsename,
+		  shouldResume: shouldResume,
+		});
+	
+		return (input: MultiStepInput) => inputGitEmail(input, state);
+	  }
+
+	  async function inputGitEmail(input: MultiStepInput, state: Partial<State>) {
+		state.gitEmail = await input.showInputBox({
+		  ignoreFocusOut: true,
+		  title,
+		  step: 2,
+		  totalSteps: 2,
+		  value: state.gitEmail || "",
+		  prompt: "Enter Git Email",
+		  validate: validateEmail,
+		  shouldResume: shouldResume,
+		});
+	
+		return terminal.sendText(`mcdev upgrade --y.user.name "${state.gitUsername}" --y.user.email "${state.gitEmail}"`);
+	  }
+	
     function shouldResume() {
         return new Promise < boolean > ((resolve, reject) => {
         });
     }
 
+	async function validateEmail(value: string) {
+		await new Promise((resolve) => setTimeout(resolve, 1000));
+		value = value.trim();
+		const regex =
+		  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		return !value || !regex.test(String(value).toLowerCase())
+		  ? "Please enter valid email"
+		  : undefined;
+	  }
+	  async function validateUsename(value: string) {
+		await new Promise((resolve) => setTimeout(resolve, 1000));
+		value = value.trim();
+		return !value ||
+		  value.trim().length < 4 ||
+		  value.includes('"') ||
+		  value.includes("'")
+		  ? "Please enter valid username"
+		  : undefined;
+	  }
 
     const state = await collectInputs();
 }
